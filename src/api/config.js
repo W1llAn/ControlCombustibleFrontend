6b73a49b8b6b4eb6d2086e394831e6af6c8d4dca
W1llAn <wchimborazo2840@uta.edu.ec>
2025-05-04 20:29:26 -0500

    Creación del archivo config para axios

---
+++ src/api/config.js
@@ -0,0 +1,33 @@
+import axios from "axios";
+
+// SE CREA EL OBJETO API PARA CONSUMIRLO CON AXIOS
+const api = axios.create({
+  baseURL: "http://localhost:5050", // API Gateway
+  timeout: 10000, // TIEMPO DE ESPERA MÁXIMO
+});
+
+// SE ENVÍA AUTOMÁTICAMENTE EL JWT
+api.interceptors.request.use((config) => {
+  const token = localStorage.getItem("jwtToken");
+
+  if (token) {
+    config.headers.Authorization = `Bearer ${token}`;
+  }
+
+  return config;
+});
+
+// SE MANEJAN LOS ERRORES
+api.interceptors.response.use(
+  (response) => response,
+  (error) => {
+    if (error.response?.status === 401) {
+      console.log("ERROR EN LA CONSULTA", error);
+      localStorage.removeItem("jwtToken"); // Eliminar el token inválido
+      window.location.href = "/login"; // Redirigir al login
+    }
+    return Promise.reject(error);
+  }
+);
+
+export default api;
