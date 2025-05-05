1ea0c61627f30669f946b367359abda13a6d8845
W1llAn <wchimborazo2840@uta.edu.ec>
2025-05-04 20:32:31 -0500

    Cambio en el App para usar el login

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,26 @@
 import { PrimeReactProvider } from "primereact/api";
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+import ProtectedRoute from "./Components/ProtectedRoute";
+import Login from "./pages/Login";
 import Home from "./pages/Home";
 function App() {
   return (
     <>
       <PrimeReactProvider>
-        <Home />
+        <BrowserRouter>
+          <Routes>
+            <Route path="/login" element={<Login />} />
+
+            {/* Todas las rutas protegidas */}
+            <Route element={<ProtectedRoute />}>
+              <Route path="/administracion" element={<Home />} />
+              <Route path="/" element={<Home />} /> // Ruta principal protegida
+            </Route>
+
+            {/* Redirección por defecto */}
+            <Route path="*" element={<Navigate to="/" replace />} />
+          </Routes>
+        </BrowserRouter>
       </PrimeReactProvider>
     </>
   );
