b9dc039b6a9cf1ccaa999053c0044c05e1c589ef
IsmaelSailema20 <ismaelsailema110@gmail.com>
2025-07-07 00:11:22 -0500

    configuraciones de despliegue

--- src/api/config.js
+++ src/api/config.js
@@ -2,8 +2,11 @@ import axios from "axios";
 
 // SE CREA EL OBJETO API PARA CONSUMIRLO CON AXIOS
 const api = axios.create({
-  baseURL: "http://localhost:5050", // API Gateway
+  baseURL: import.meta.env.VITE_API_URL,
   timeout: 10000, // TIEMPO DE ESPERA MÁXIMO
+  headers: {
+    "ngrok-skip-browser-warning": "true", // Añade este encabezado
+  },
 });
 
 // SE ENVÍA AUTOMÁTICAMENTE EL JWT
